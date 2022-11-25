2ab5bde94ef3a733ea8726e39fcbe59d5d357d83
Lovsog <840018730@qq.com>
2022-11-25 21:04:31 +0800

    28.Spring-Cloud-Eureka-Server-Discovery

---
+++ _28_Spring_Cloud_Eureka_Server_Discovery/Eureka-Client/src/test/java/com/lspringcloud/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lspringcloud.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
